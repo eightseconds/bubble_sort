568113d1f28a122c2e9931048282c9f72f7eed12
eightseconds <paulseokcho@gmail.com>
2019-09-30 16:23:07 -0700

    Update

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,7 +2,6 @@ function bubbleSort(array) {
   let sorted = false
 	while (!sorted) {
 		sorted = true
-		
 		for(let i = 0; i < array.length - 1; i++) {
 			if (array[i] > array[i + 1]) {
 				let swap = array[i + 1]
